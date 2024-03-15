c499d7f6b8969f56e5b258d265f0b84e305d9d3e
adrianaValo <adriana.olteanu14@gmail.com>
2024-03-15 10:06:35 +0200

    Session 20

--- REACT_Projekte/yt-clone/yt-clone/src/utils/fetchFromAPI.js
+++ REACT_Projekte/yt-clone/yt-clone/src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    "X-RapidAPI-Key": "c28e881f5fmsh07da099f930997ep1941e3jsn5fc09739323a",
+    "X-RapidAPI-Key": "create the file with the API Key",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
---
+++ Session 20- Proiect/my-movie/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import MovieApp from './components/MovieApp';
+
+function App() {
+  return (
+    <div className='App'>
+      <MovieApp />
+    </div>
+  );
+}
+
+export default App;
