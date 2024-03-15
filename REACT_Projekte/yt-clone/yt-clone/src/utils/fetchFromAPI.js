8426a3382a863efd3e6112523710cf89ad4c19e6
adrianaValo <adriana.olteanu14@gmail.com>
2024-03-15 15:33:50 +0200

    Proiect Functionality

--- REACT_Projekte/yt-clone/yt-clone/src/utils/fetchFromAPI.js
+++ REACT_Projekte/yt-clone/yt-clone/src/utils/fetchFromAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com/captions";
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    "X-RapidAPI-Key": "create the file with the API Key",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
---
+++ Streamify- Video_React_Proiect/streamify-video/src/App.js
@@ -0,0 +1,15 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+import MovieApp from "./components/MovieApp";
+
+function App() {
+  return (
+    <div className='App'>
+      <MovieApp />
+    </div>
+  );
+}
+
+export default App;
