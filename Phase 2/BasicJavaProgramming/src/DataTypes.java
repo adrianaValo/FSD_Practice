4d65d2eff66ff4affd1a33be5d8c084fb782dcff
adrianaValo <adriana.olteanu14@gmail.com>
2024-04-25 14:29:15 +0300

    Session1

---
+++ Phase 2/BasicJavaProgramming/src/DataTypes.java
@@ -0,0 +1,17 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=100;
+		double a=10.10;
+		char c='%';
+		boolean res = true;
+		System.out.println(n);
+		System.out.println("The value of n is "+n);
+		System.out.println("The value of a is "+a);
+		System.out.println("The value of c is "+c);
+		System.out.println("The value of res is "+res);
+	}
+
+}
---
+++ Phase 2/BasicJavaProgramming/src/Typecasting.java
@@ -0,0 +1,29 @@
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		byte a=10;
+//		short b=a;		// implicit type casting 
+//		System.out.println("a "+a);
+//		System.out.println("b "+b);
+//		
+//		short c = 10;
+//		byte d = (byte)c;	// explicit type casting 
+//		System.out.println("c "+c);
+//		System.out.println("d "+d);
+		
+		int a=100;
+		float b=a;			// implicit type casting 
+		System.out.println("a "+a);
+		System.out.println("b "+b);
+
+		//float c = 100.10f;		//explicit type casting converting double to float 
+		// float c = (float)100.0;		//explicit type casting converting double to float
+		double c = 100.0;	
+		int d = (int)c;		// explicit type casting 
+		System.out.println("c "+c);
+		System.out.println("d "+d);
+	}
+
+}
