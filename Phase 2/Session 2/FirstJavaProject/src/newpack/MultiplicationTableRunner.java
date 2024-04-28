a9ba1424914a81743c09762c3031d0e26b5c061d
adrianaValo <adriana.olteanu14@gmail.com>
2024-04-28 15:21:00 +0300

    MultiplicationTable Example

---
+++ Phase 2/Session 2/FirstJavaProject/src/newpack/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package newpack;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+
+	}
+
+}
