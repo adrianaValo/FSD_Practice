ec9e7bf2fd32121a87d515b4bf7fbfa2a50e58c0
adrianaValo <adriana.olteanu14@gmail.com>
2024-05-07 11:17:49 +0300

    07/05 first part

--- Phase 2/Collection-Framework-Examples/src/com/MapDemo.java
+++ Phase 2/Collection-Framework-Examples/src/com/MapDemo.java
@@ -1,12 +1,28 @@
 package com;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class MapDemo {
 
 	public static void main(String[] args) {
 		// HashMap Example
-		HashMap hm = new HashMap(); //unordered
+//		HashMap hm = new HashMap(); //unordered
+//		hm.put(2,  "Steven");
+//		hm.put(3,  "John");
+//		hm.put(4,  "Lex");
+//		hm.put(6,  "Neena");
+//		hm.put("A",  "B");
+//		System.out.println(hm);
+//		
+//		hm.put(2, "Akash");
+//		System.out.println(hm);
+//		System.out.println("get value " + hm.get(2));
+//		System.out.println("present key " + hm.containsKey(1));
+//		System.out.println("present value " + hm.containsValue("Neena"));
+		
+		// LinkedHashmap example
+		LinkedHashMap hm = new LinkedHashMap(); //unordered
 		hm.put(2,  "Steven");
 		hm.put(3,  "John");
 		hm.put(4,  "Lex");
