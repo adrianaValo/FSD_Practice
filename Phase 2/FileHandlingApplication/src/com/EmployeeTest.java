31997c9695c0d37aab4a6af1021873eef0c9dd3c
adrianaValo <adriana.olteanu14@gmail.com>
2024-05-09 09:13:15 +0300

    Session 09/05

---
+++ Phase 2/FileHandlingApplication/src/com/EmployeeTest.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) throws Exception{
+//	Employee emp1 = new Employee(100, "Steven", 45000);
+//	System.out.println(emp1);
+//	// store object in file 
+//	FileOutputStream fos = new FileOutputStream("employee.txt");
+//	ObjectOutputStream oos = new ObjectOutputStream(fos);
+//	oos.writeObject(emp1);
+//	System.out.println("object serialization done successfully");
+		
+	FileInputStream fis = new FileInputStream("employee.txt");
+	ObjectInputStream ois = new ObjectInputStream(fis);
+	Object obj = ois.readObject();	// return type is object class reference. 
+	Employee emp2 = (Employee)obj;
+	System.out.println(emp2);
+	}
+
+}
