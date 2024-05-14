90d33d18c9e1eb11feb346d2072d06d9a4b1a128
adrianaValo <adriana.olteanu14@gmail.com>
2024-05-14 10:30:19 +0300

    Session 13/05+ Setup for the Maven App

--- Phase 2/Java Web Applications/ServletApp/src/com/LoginServlet.java
+++ Phase 2/Java Web Applications/ServletApp/src/com/LoginServlet.java
@@ -3,6 +3,7 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,19 +33,38 @@ public class LoginServlet extends HttpServlet {
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
 		//pw.println("Your emailid is "+emailid);
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");   // output consider as html rather than plain text 
 		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
-			pw.print("successfully login");
+			pw.print("successfully login  with get method");
+			rd1.forward(request, response);			// we can see the output for target page 
 		}else {
-			pw.println("failure try once again");
+			pw.println("failure try once again with get method");
+			rd2.include(request, response);      // both failure message + login.html page included 
 		}
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter pw = response.getWriter();
+		String emailid = request.getParameter("emailid");
+		String password = request.getParameter("password");
+		//pw.println("Your emailid is "+emailid);
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");   // output consider as html rather than plain text 
+		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
+			pw.print("successfully login with post method");
+			rd1.forward(request, response);
+		}else {
+			pw.println("failure try once again with post method");
+			rd2.include(request, response);
+		}
+
 	}
 
 }
