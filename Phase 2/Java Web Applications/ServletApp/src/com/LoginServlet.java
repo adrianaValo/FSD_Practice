03421a7f010f1feb6a5169df1746fa2437c4e824
adrianaValo <adriana.olteanu14@gmail.com>
2024-05-14 08:24:51 +0300

    Session 13/05

---
+++ Phase 2/Java Web Applications/ServletApp/src/com/LoginServlet.java
@@ -0,0 +1,50 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		String emailid = request.getParameter("emailid");
+		String password = request.getParameter("password");
+		//pw.println("Your emailid is "+emailid);
+		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
+			pw.print("successfully login");
+		}else {
+			pw.println("failure try once again");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
